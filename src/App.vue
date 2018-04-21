fdfea3f4e238e26bcf7435d076030d5d581a6c63
Forrest Wilson <forrest.wilson@me.com>
2018-04-21 14:24:34 +1200

    Initial setup

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,12 @@
 
 <script>
     export default {
-        
+        data: function() {
+            return {
+                quotes: ["Just a quote to see something"],
+                maxQuotes: 10
+            };
+        }
     }
 </script>
 

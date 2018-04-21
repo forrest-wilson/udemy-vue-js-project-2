0f727185fde3a952795e530277dfa9392dcd0c5d
Forrest Wilson <forrest.wilson@me.com>
2018-04-21 16:29:51 +1200

    Added a limit to the number of quotes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <app-header></app-header>
+        <app-header :quoteCount = "quotes.length" :maxQuotes="maxQuotes"></app-header>
         <app-new-quote @quoteAdded="newQuote"></app-new-quote>
         <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
         <div class="row">
@@ -30,6 +30,9 @@
         },
         methods: {
             newQuote(quote) {
+                if (this.quotes.length >= this.maxQuotes) {
+                    return alert("Please delete quotes first");
+                }
                 this.quotes.push(quote);
             },
             deleteQuote(i) {
